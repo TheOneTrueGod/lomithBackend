51a0d63a20aa2049fb2957c6801721e02c5668ef
Jeremy Prevoe <jprevoe@gmail.com>
2026-01-01 15:22:44 -0800

    Another fix attempt

--- apps/auth/apps.py
+++ apps/auth/apps.py
@@ -4,4 +4,5 @@ from django.apps import AppConfig
 class AuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.auth'
+    label = 'user_auth'  # Custom label to avoid conflict with django.contrib.auth
 
